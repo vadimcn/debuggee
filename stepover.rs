504b6ec09fe3e83c91b287b4543ef855af0ed4e5
Vadim Chugunov <vadimcn@gmail.com>
2016-01-24 21:33:55 -0800

    WIP

--- stepover.rs
+++ stepover.rs
@@ -1,34 +1,38 @@
+#![allow(bad_style)]
 #![feature(test)]
 extern crate test;
 
 #[inline(never)]
-fn xxx(x: i32) {
+fn x__________x(x: i32) {
 	test::black_box(x);
 }
 
-macro_rules! quox{
-    ($a:expr) => {
-		xxx($a + 1);
-		xxx($a + 2);
-		xxx($a + 3);
-     }
-}
+// macro_rules! quox{
+//     ($a:expr) => {
+// 		x__________x($a + 1);
+// 		x__________x($a + 2);
+// 		x__________x($a + 3);
+//      }
+// }
 
-#[inline]
-fn inlined(x: i32) {
-    xxx(x+1);
-    xxx(x+2);
-    xxx(x+3);
-}
+// #[inline]
+// fn inlined(x: i32) {
+//     x__________x(x+1);
+//     x__________x(x+2);
+//     x__________x(x+3);
+// }
 
 #[inline(never)]
 fn main() {
-    xxx(1);
-    xxx(10);
-    inlined(10);
-    xxx(20);
-    quox!(20);
-    xxx(30);
-    println!("Hello {}", "world");
-    xxx(40);
+    x__________x(1);
+    // x__________x(10);
+    // inlined(10);
+    // x__________x(20);
+    // quox!(20);
+    x__________x(30);
+    //println!("Hello {}", "world");
+    println!("Hello world");
+    x__________x(40);
+    // println!("Hello {}", "world");
+    // x__________x(50);    
 }

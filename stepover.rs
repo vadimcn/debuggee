fc064d9f068edcfe233d9703c33368643a6d1632
Vadim Chugunov <vadimcn@gmail.com>
2016-06-30 18:17:13 -0700

    Zorbit

--- stepover.rs
+++ stepover.rs
@@ -1,38 +1,42 @@
 #![allow(bad_style)]
+#![allow(unused)]
+#![allow(private_no_mangle_fns)] 
 #![feature(test)]
 extern crate test;
 
+#[no_mangle]
 #[inline(never)]
-fn x__________x(x: i32) {
-	test::black_box(x);
+fn x__________x(label: i32) {
+	test::black_box(label);
 }
 
-// macro_rules! quox{
-//     ($a:expr) => {
-// 		x__________x($a + 1);
-// 		x__________x($a + 2);
-// 		x__________x($a + 3);
-//      }
-// }
+macro_rules! quox{
+    ($($a:stmt;)*) => { $($a;)* }
+}
 
-// #[inline]
-// fn inlined(x: i32) {
-//     x__________x(x+1);
-//     x__________x(x+2);
-//     x__________x(x+3);
-// }
+macro_rules! quox2{
+    ($($a:stmt;)*) => { quox!($($a;)*) }
+}
+
+#[inline(always)]
+fn inlined<T>(x: T) {
+    x__________x(101);
+    test::black_box(x);
+    x__________x(199);
+}
 
 #[inline(never)]
 fn main() {
+    x__________x(0);
     x__________x(1);
-    // x__________x(10);
-    // inlined(10);
-    // x__________x(20);
-    // quox!(20);
-    x__________x(30);
-    println!("Hello {}", "world");
-    //println!("Hello world");
-    x__________x(40);
-    // println!("Hello {}", "world");
-    // x__________x(50);    
+    inlined(10);
+    x__________x(2);
+    quox2!{
+        let a = 1;
+    }
+    x__________x(3);
+    let x = vec![42];
+    x__________x(4);
+    println!("Hello world");
+    x__________x(999);    
 }

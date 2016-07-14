38c91bb934aa3e4224ece8a9f668616a29a0f5e0
Vadim Chugunov <vadimcn@gmail.com>
2016-07-13 17:39:36 -0700

    more unwind

--- unwind.rs
+++ unwind.rs
@@ -1,11 +1,24 @@
 use std::panic;
 
+fn bar() {
+    println!("bar");
+    panic!("Всем стоятъ боятъся!!!");
+}
+
+fn foo() {
+    println!("foo");
+    bar();
+}
+
 fn main() {
 
     let x = panic::catch_unwind(|| {
-        println!("Foo");
-        panic!("Всем стоятъ боятъся!!!");
+        println!("closure");
+        foo();
+        println!("closure after");
     });
     
-    println!("Caught {:?}", x); 
+    let x = x.err().unwrap();
+    let y = x.downcast_ref::<&str>().unwrap();  
+    println!("caught `{}`", y); 
 }

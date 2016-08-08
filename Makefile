341dd98bea37f7d63c74f224b15147ab84954363
VC <vadimcn@gmail.com>
2016-08-08 15:19:20 -0700

    llrust

--- Makefile
+++ Makefile
@@ -6,10 +6,11 @@ MKDIR = mkdir -p
 MOVE = mv
 
 $(WORKSPACE_ROOT)/out/%.cpp/target.exe: $(WORKSPACE_ROOT)/%.cpp
-	@-$(MKDIR) out/$*.rs
-	clang++ -std=c++11 -pthread -g $(WORKSPACE_ROOT)/$*.cpp -o $(WORKSPACE_ROOT)/out/$*.cpp/target.exe
+	@-$(MKDIR) out/$*.cpp
+	clang++ -std=c++11 -pthread -g $(WORKSPACE_ROOT)/$*.cpp -O0 -S -emit-llvm -o $(WORKSPACE_ROOT)/out/$*.cpp/$*.ll
+	clang++ -std=c++11 -pthread -g $(WORKSPACE_ROOT)/$*.cpp -O0 -o $(WORKSPACE_ROOT)/out/$*.cpp/target.exe
 
 $(WORKSPACE_ROOT)/out/%.rs/target.exe: $(WORKSPACE_ROOT)/%.rs
 	@-$(MKDIR) out/$*.rs
-	rustc -g $(WORKSPACE_ROOT)/$*.rs -Zorbit --emit=llvm-ir,asm,link --out-dir $(WORKSPACE_ROOT)/out/$*.rs
+	rustc -g $(WORKSPACE_ROOT)/$*.rs --emit=llvm-ir,asm,link --out-dir $(WORKSPACE_ROOT)/out/$*.rs -Copt-level=0
 	$(MOVE) $(WORKSPACE_ROOT)/out/$*.rs/$* $(WORKSPACE_ROOT)/out/$*.rs/target.exe 
--- util.py
+++ util.py
@@ -2,18 +2,17 @@ import lldb
 
 def print_block(block, level):
     indent = '  ' * level
-    print indent, str(block)
-    for var in block.GetVariables(lldb.frame, True, True, False, lldb.eNoDynamicValues):
-        print indent, var.type, var.name, '=', var.value #, str(var)
+    print indent, str(block), '-', str(block.GetInlinedName())
+    for var in block.GetVariables(lldb.target, True, True, True):
+        print indent, var.type, var.name
 
-def visit_block(block, level=0):
-    while block.IsValid():
-        print_block(block, level)
-        child = block.GetFirstChild()
-        if child.IsValid():
-            visit_block(child, level+1)
-        block = block.GetSibling()
-        break
+def visit_block(block):
+    parent = block.GetParent()
+    level = 0
+    if parent.IsValid():
+        level = visit_block(parent)
+    print_block(block, level)
+    return level + 1
 
 def main():
     visit_block(lldb.frame.block)

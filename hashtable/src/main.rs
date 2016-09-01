63a089866c41e874d5b4535ae6839f2644b36ffa
VC <vadimcn@gmail.com>
2016-08-31 23:09:08 -0700

    hashtable iters

--- hashtable/src/main.rs
+++ hashtable/src/main.rs
@@ -1,15 +1,19 @@
 #![feature(test)]
 extern crate test;
 
-fn main() {
+fn test(iters: u32) {
     let mut m = std::collections::HashMap::<u32, u32>::new();
-    for i in 0..100_000 {
+    for i in 0..iters {
         *m.entry(i % 1000).or_insert(0) += 1;
     }
     test::black_box(m[&400]);
 }
 
+fn main() {
+    test(200_000_000);
+}
+
 #[bench]
 fn bench(b: &mut test::Bencher) {
-    b.iter(|| main());
+    b.iter(|| test(100_000));
 }

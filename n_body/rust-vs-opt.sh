c16cc34ae41b1e2df91234d25a4685a1bb3f53d8
VC <vadimcn@gmail.com>
2016-09-02 23:51:43 -0700

    -O3

--- n_body/rust-vs-opt.sh
+++ n_body/rust-vs-opt.sh
@@ -3,10 +3,11 @@ set -v
 
 rm n_body*
 
-LINK=`rustc src/main.rs -O -o n_body-rustc -Zprint-link-args`
+LINK=`rustc src/main.rs -Copt-level=3 -o n_body-rustc -Zprint-link-args`
 
-rustc src/main.rs --emit=llvm-ir -o n_body.ll
-opt n_body.ll -O2 -S -o n_body-opt.ll
+rustc src/main.rs --emit=llvm-ir -o n_body.ll -Copt-level=3
+opt n_body.ll -O3 -S -o n_body-opt.ll
+opt n_body-opt.ll -O3 -S -o n_body-opt.ll
 llc n_body-opt.ll -filetype=obj -o n_body-opt.o
 eval ${LINK/n_body-rustc.0.o/n_body-opt.o} -o n_body-opt
 

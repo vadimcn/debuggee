23ff2bef18f6e3c3c835927d8f05f3989060e4cb
VC <vadimcn@gmail.com>
2016-09-02 23:51:59 -0700

    clang-pgo

--- n_body/clang-pgo.sh
+++ n_body/clang-pgo.sh
@@ -1,33 +1,42 @@
 
-TARGET=x86_64-apple-darwin
-HASH=836a4172
-RUSTLIBS="-L $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/libstd-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/libpanic_unwind-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/libunwind-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/librand-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/libcollections-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/librustc_unicode-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/liballoc-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/liballoc_jemalloc-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/liblibc-$HASH.rlib $HOME/NW/rust/build/$TARGET/stage1/lib/rustlib/$TARGET/lib/libcore-$HASH.rlib"
-
-function link {
-    cc -m64 $1 -o $2 -Wl,-dead_strip -nodefaultlibs $RUSTLIBS -L$HOME/NW/cargo-pgo/target/debug -lprofiler-rt -lSystem -lpthread -lc -lm -lcompiler-rt
-}
-
-
 set -v
 
-rm default.profraw pgo.profdata unopt pgo-instr pgo-opt *.bc *.ll *.o
+rm pgo.prof* *.bc *.ll *.o n_body*
 
-rustc src/main.rs -O --emit=llvm-ir -o n_body.ll
+# plain
+LINK=`rustc src/main.rs -Copt-level=3 -o n_body -Zprint-link-args`
 
-opt n_body.ll -O2 -profile-generate=pgo.profraw -S -o pgo-instr.ll
-llc pgo-instr.ll -filetype=obj -o pgo-instr.o
-link pgo-instr.o pgo-instr
+# instrumented
+rustc src/main.rs -Copt-level=3 -Cllvm-args=-profile-generate=pgo.profraw -Lnative=$HOME/NW/cargo-pgo/target/debug -Clink-args=-lprofiler-rt -o n_body-instr
 
-./pgo-instr 100000
+# rustc src/main.rs -Copt-level=3 --emit=llvm-ir -o n_body.ll 
+# opt n_body.ll -O3 -profile-generate=pgo.profraw -S -o pgo-instr.ll
+# llc pgo-instr.ll -filetype=obj -o n_body-instr.o
+# eval ${LINK/n_body.0.o/n_body-instr.o} -o n_body-instr -L$HOME/NW/cargo-pgo/target/debug -lprofiler-rt
+
+./n_body-instr 100000
 llvm-profdata merge pgo.profraw -output=pgo.profdata
 
-opt n_body.ll -O2 -profile-use=pgo.profdata  -S -o pgo-opt.ll
-llc pgo-opt.ll -filetype=obj -o pgo-opt.o
-link pgo-opt.o pgo-opt
+# rust pgo
+rustc src/main.rs -Copt-level=3 -o n_body-rustc -Cllvm-args=-profile-use=pgo.profdata
 
-rustc -O src/main.rs -o unopt
-time ./unopt 20000000
+# opt pgo
+rustc src/main.rs -Copt-level=3 --emit=llvm-ir -o n_body.ll #-Cllvm-args=-profile-use=pgo.profdata
+opt n_body.ll -O3 -S -o n_body-opt.ll -profile-use=pgo.profdata
+opt n_body-opt.ll -O3 -S -o n_body-opt2.ll
+llc n_body-opt2.ll -filetype=obj -o n_body-opt.o
+eval ${LINK/n_body.0.o/n_body-opt.o} -o n_body-opt
 
-time ./pgo-opt 20000000
+# multiopt
+rustc src/main.rs -Copt-level=3 --emit=llvm-ir -o n_body.ll 
+opt n_body.ll -O3 -S -o n_body-multiopt.ll 
+opt n_body-multiopt.ll -O3 -S -o n_body-multiopt.ll
+llc n_body-multiopt.ll -filetype=obj -o n_body-multiopt.o
+eval ${LINK/n_body.0.o/n_body-multiopt.o} -o n_body-multiopt
+
+time ./n_body 20000000
+time ./n_body-rustc 20000000
+time ./n_body-opt 20000000
+time ./n_body-multiopt 20000000
 
 set +v
